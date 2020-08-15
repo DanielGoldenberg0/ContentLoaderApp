3f2778ea0c99fc42f25ffe5bc493e7c7b57f8339
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2020-08-14 21:49:05 -0400

    Added

--- server/server.go
+++ server/server.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"fmt"
-	// "github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+var path string = "data.json"
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/load", loadUsers).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
+
+	info()
+}
+
+func loadUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	data, err := ioutil.ReadFile("./" + path)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(data[0])
+}
 
 func info() {
 	fmt.Println("The server is active")
 	fmt.Println(" * IP: localhost")
 	fmt.Println(" * Port: 8080")
-}
-
-func main() {
-	info()
 }
