d6ec7433450a6ed660eb5a91b74fbf8cbc9458d0
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2020-08-14 23:44:52 -0400

    Implemented fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import User from "./components/User.js"
 
-const App = () => {
-  return (
-    <div className="container">
-      <h1 id="title">Content Loader</h1>
-      <button>Load</button>
-      <User/>
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    loading: true,
+    user: null
+  }
+
+  async loadUsers() {
+    var url = "http://localhost:8080/api/load"
+    var response = await fetch(url)
+    var users = await response.json()
+    this.setState({
+      user: users[0]
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 id="title">Content Loader</h1>
+        <button onClick={() => this.loadUsers()}>Load</button>
+        {this.state.loading || !this.state.user 
+        ? "" 
+        : <User username={this.state.user.username} email={this.state.user.email} id={this.state.user.id}/>}
+      </div>
+    )
+  }
 }
 
 export default App
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const User = () => {
-  return (
-    <div className="user-box">
-      <div style={{marginBottom: 10}}>
-        <h2>Username</h2>
+class User extends React.Component {
+  render() {
+    return (
+      <div className="user-box">
+        <div style={{marginBottom: 10}}>
+        <h2>{this.props.username}</h2>
+        </div>
+        <h3>{this.props.email}</h3>
+        <h3>{this.props.id}</h3>
       </div>
-      <h3>Email</h3>
-      <h3>ID</h3>
-    </div>
-  )
+    )
+  }
 }
 
 export default User
